723fa8dc8c63a6d663b39c8548e105cb9ae622d2
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-06-15 22:54:41 +0300

    phase-1-javascript-functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// razzle();
+// function razzle (){
+//     console.log("You've been razzled!");
+// }
+// function razzle (lawyer = "Billy", target = "'em") {
+//     console.log(`${lawyer} razzle-dazzles ${target}!`);
+// }
+// razzle();
+// razzle("Methuselah", "T'challah");
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+consologe.log(saturdayFun());
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+console.log(mondayWork());
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    }
+}
+console.log(wrapAdjective()("a hard worker"));
